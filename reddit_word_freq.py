40852b3d9b3f02c6e86c48c8e640d4d53c72844d
Michelle Xia <48334308+michelle-xia@users.noreply.github.com>
2020-10-25 14:40:55 -0500

    Delete reddit_word_freq.py

--- reddit_word_freq.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-stop = stopwords.words('english')
-df = pd.read_csv('aggregate_reddit_text.csv', engine='python')
-df.rename(columns=lambda x: x.strip(), inplace=True)
-print(df)
-            
